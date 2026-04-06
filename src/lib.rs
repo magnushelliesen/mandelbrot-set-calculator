43775294735b3fd17e715266a719235a778a2c75
magnushelliesen <magnus.helliesen@gmail.com>
2026-04-06 11:09:50 +0200

    Starten på test av puntk

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,31 @@ use pyo3::prelude::*;
 /// A Python module implemented in Rust.
 #[pymodule]
 mod mandelbrot_calculator {
+    use std::f64;
     use pyo3::prelude::*;
 
     /// Formats the sum of two numbers as string.
     #[pyfunction]
-    fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-        Ok((a + b).to_string())
+    fn is_in_mandelbrot_set(c_re: f64, c_im: f64, n: i64) -> PyResult<bool> {
+        let z_re_1: f64 = 0.0;
+        let z_im_1: f64 = 0.0;
+        let _z_re_0: f64 = 0.0;
+        let _z_im_0: f64 = 0.0;
+        for _ in 0..n {
+            let z_re_1 = _z_re_0.powf(2.0)-_z_im_0.powf(2.0) + c_re;
+            let z_im_1 = 2.0 * _z_re_0 * _z_im_0 + c_im;
+            let _z_re_0 = z_re_1;
+            let _z_im_0 = z_im_1;
+
+            print!("{} {}\n", z_re_1, z_im_1);
+        }
+
+        Ok(
+            if (z_re_1.powf(2.0) + z_im_1.powf(2.0)).sqrt() < 2.0 {
+                true
+            } else {
+                false
+            }
+        )
     }
 }
