222bd85e62bee2beeacc757217d9f7479f6e2d79
magnushelliesen <magnus.helliesen@gmail.com>
2026-04-06 11:30:13 +0200

    Kommentarer

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,15 @@ mod mandelbrot_calculator {
     use std::f64;
     use pyo3::prelude::*;
 
-    /// Formats the sum of two numbers as string.
     #[pyfunction]
     fn is_in_mandelbrot_set(re: f64, im: f64, n: i64) -> PyResult<bool> {
+        // Setup variabels
         let mut z_re: f64 = 0.0;
         let mut z_im: f64 = 0.0;
         let _z_re: f64 = 0.0;
         let _z_im: f64 = 0.0;
 
+        // Do n iterations of sequence
         for _ in 0..n {
             z_re =_z_re.powf(2.0)-_z_im.powf(2.0) + re + z_re;
             z_im = 2.0 * _z_re * _z_im + im;
@@ -21,6 +22,7 @@ mod mandelbrot_calculator {
             let _z_im = z_im;
         }
 
+        // Return true if absolute value of z <= 2
         Ok(
             if (z_re.powf(2.0) + z_im.powf(2.0)).sqrt() <= 2.0 {
                 true
