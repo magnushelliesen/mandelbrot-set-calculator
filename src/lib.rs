fa1efb6f3f08ce954e70ff21483f9eba2a0988b8
magnushelliesen <magnus.helliesen@gmail.com>
2026-04-06 11:12:59 +0200

    Starten på test av punkt

--- src/lib.rs
+++ src/lib.rs
@@ -8,14 +8,14 @@ mod mandelbrot_calculator {
 
     /// Formats the sum of two numbers as string.
     #[pyfunction]
-    fn is_in_mandelbrot_set(c_re: f64, c_im: f64, n: i64) -> PyResult<bool> {
-        let z_re_1: f64 = 0.0;
-        let z_im_1: f64 = 0.0;
-        let _z_re_0: f64 = 0.0;
-        let _z_im_0: f64 = 0.0;
+    fn is_in_mandelbrot_set(re: f64, im: f64, n: i64) -> PyResult<bool> {
+        let z_re: f64 = 0.0;
+        let z_im: f64 = 0.0;
+        let _z_re: f64 = 0.0;
+        let _z_im: f64 = 0.0;
         for _ in 0..n {
-            let z_re_1 = _z_re_0.powf(2.0)-_z_im_0.powf(2.0) + c_re;
-            let z_im_1 = 2.0 * _z_re_0 * _z_im_0 + c_im;
+            let z_re_1 = _z_re.powf(2.0)-_z_im.powf(2.0) + re;
+            let z_im_1 = 2.0 * _z_re * _z_im + im;
             let _z_re_0 = z_re_1;
             let _z_im_0 = z_im_1;
 
@@ -23,7 +23,7 @@ mod mandelbrot_calculator {
         }
 
         Ok(
-            if (z_re_1.powf(2.0) + z_im_1.powf(2.0)).sqrt() < 2.0 {
+            if (z_re.powf(2.0) + z_im.powf(2.0)).sqrt() < 2.0 {
                 true
             } else {
                 false
