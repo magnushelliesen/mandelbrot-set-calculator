fb0db2507dbc475eb5ab6b1d4df362a434661fd8
magnushelliesen <magnus.helliesen@gmail.com>
2026-04-12 09:52:45 +0200

    Lintet

--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,20 @@ use pyo3::prelude::*;
 /// A Python module implemented in Rust.
 #[pymodule]
 mod mandelbrot_calculator {
-    use std::f64;
     use pyo3::prelude::*;
     use rayon::prelude::*;
+    use std::f64;
 
     #[pyclass(get_all, set_all)]
     struct MandelbrotSet {
-        grid_size: usize
+        grid_size: usize,
     }
 
     #[pymethods]
     impl MandelbrotSet {
         #[new]
         fn new(grid_size: usize) -> Self {
-            MandelbrotSet {grid_size}
+            MandelbrotSet { grid_size }
         }
 
         fn make_grid(
@@ -25,17 +25,21 @@ mod mandelbrot_calculator {
             re_max: f64,
             im_min: f64,
             im_max: f64,
-            max_iter: i32
+            max_iter: i32,
         ) -> Vec<Vec<i32>> {
             let mut grid = vec![vec![0; self.grid_size]; self.grid_size];
 
+            #[allow(clippy::needless_range_loop)]
             for row in 0..self.grid_size {
                 for col in 0..self.grid_size {
                     // Calculate the re and im part for point
-                    let re = re_min + (col as f64 / (self.grid_size - 1) as f64) * (re_max - re_min);
-                    let im = im_min + (row as f64 / (self.grid_size - 1) as f64) * (im_max - im_min);
+                    let re =
+                        re_min + (col as f64 / (self.grid_size - 1) as f64) * (re_max - re_min);
+                    let im =
+                        im_min + (row as f64 / (self.grid_size - 1) as f64) * (im_max - im_min);
 
-                    grid[self.grid_size - row - 1][col] = _iteration_at_which_point_explodes(re, im, max_iter);
+                    grid[self.grid_size - row - 1][col] =
+                        _iteration_at_which_point_explodes(re, im, max_iter);
                 }
             }
 
@@ -49,36 +53,34 @@ mod mandelbrot_calculator {
             re_max: f64,
             im_min: f64,
             im_max: f64,
-            max_iter: i32
+            max_iter: i32,
         ) -> Vec<Vec<i32>> {
-            py.detach(||
-                {
-                    (0..self.grid_size)
-                        .into_par_iter()
-                        .map(|row| {
-                            (0..self.grid_size)
-                                .map(|col| {
-                                    // Calculate the re and im part for point
-                                    let re = re_min + (col as f64 / (self.grid_size - 1) as f64) * (re_max - re_min);
-                                    let im = im_min + (row as f64 / (self.grid_size - 1) as f64) * (im_max - im_min);
+            py.detach(|| {
+                (0..self.grid_size)
+                    .into_par_iter()
+                    .map(|row| {
+                        (0..self.grid_size)
+                            .map(|col| {
+                                // Calculate the re and im part for point
+                                let re = re_min
+                                    + (col as f64 / (self.grid_size - 1) as f64)
+                                        * (re_max - re_min);
+                                let im = im_min
+                                    + (row as f64 / (self.grid_size - 1) as f64)
+                                        * (im_max - im_min);
 
-                                    // Return what at what iteration the point blows up at 
-                                    _iteration_at_which_point_explodes(re, im, max_iter)
-                                })
-                                .collect::<Vec<i32>>()
-                        })
-                        .rev()
-                        .collect()
-                }
-            )
+                                // Return what at what iteration the point blows up at
+                                _iteration_at_which_point_explodes(re, im, max_iter)
+                            })
+                            .collect::<Vec<i32>>()
+                    })
+                    .rev()
+                    .collect()
+            })
         }
     }
 
-    fn _iteration_at_which_point_explodes(
-        re: f64,
-        im: f64,
-        max_iter: i32
-    ) -> i32 {
+    fn _iteration_at_which_point_explodes(re: f64, im: f64, max_iter: i32) -> i32 {
         let mut z_re = 0.0;
         let mut z_im = 0.0;
 
@@ -98,11 +100,7 @@ mod mandelbrot_calculator {
     }
 
     #[pyfunction]
-    fn is_in_mandelbrot_set(
-        re: f64,
-        im: f64,
-        max_iter: i32
-    ) -> PyResult<bool> {
+    fn is_in_mandelbrot_set(re: f64, im: f64, max_iter: i32) -> PyResult<bool> {
         Ok(_iteration_at_which_point_explodes(re, im, max_iter) <= max_iter)
     }
 }
